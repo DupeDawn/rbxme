c8496132acbfd0a7e994be1752244d99110d93df
DupeDawn <95935088+DupeDawn@users.noreply.github.com>
2022-08-04 18:38:16 +0100

    Add files via upload

---
+++ spawnlocat.lua
@@ -0,0 +1,9 @@
+function teleportTO(placeCFrame)
+    local plyr = game.Players.LocalPlayer;
+    if plyr.Character then
+         plyr.Character.HumanoidRootPart.CFrame = placeCFrame;
+    end
+    
+end
+
+ teleportTO(game:GetService("Workspace").Region_Main.SpawnLocation.CFrame)
